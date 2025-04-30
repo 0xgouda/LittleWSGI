ceff358cada220c537045ee4638f5d6211f086af
0xgouda <ahmedabdelmonaem@std.mans.edu.eg>
2025-04-30 15:18:40 +0300

    fix: empty dict in case of non-dict json object
    this avoids having a crash and instead raises internal server error

--- src/request.py
+++ src/request.py
@@ -7,8 +7,8 @@ class Request:
             setattr(self, key.replace('.', '_').lower(), value)
 
         self.casted_params = {}
-        self.body_params = {}
         self.url_params = {}
+        self.body_params = {}
 
         for key, values_list in parse_qs(self.query_string).items():
             self.url_params[key] = values_list[-1] 
@@ -16,4 +16,6 @@ class Request:
         request_body_size = int(getattr(self, 'content_length', 0))
         if request_body_size > 0:
             request_body = self.wsgi_input.read(request_body_size)
-            self.body_params = json.loads(request_body.decode())
+            body_params = json.loads(request_body.decode())
+            if isinstance(body_params, dict):
+                self.body_params = body_params
